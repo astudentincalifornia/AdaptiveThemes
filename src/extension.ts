909701a88310305650395c4a17710f395ff45ba5
Yilin Zheng <127905016+astudentincalifornia@users.noreply.github.com>
2025-05-31 04:32:07 +0000

    yes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,57 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "adaptivethemes" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			'adaptiveTheme',
+			new AdaptiveSidebarProvider(context)
+		)
+	);
 	const disposable = vscode.commands.registerCommand('adaptivethemes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from SomeAdaptiveThemes!');
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+class AdaptiveSidebarProvider implements vscode.WebviewViewProvider{
+	public static readonly viewType = '';
+
+	constructor(private context: vscode.ExtensionContext) {}
+
+	resolveWebviewView(webviewView: vscode.WebviewView, 
+		context: vscode.WebviewViewResolveContext, 
+		token: vscode.CancellationToken){
+			webviewView.webview.options = {
+				enableScripts: true,
+			};
+			webviewView.webview.html = this.getHtmlForWebview();
+			webviewView.webview.onDidReceiveMessage((message) => {
+				if (message.command === 'switchTheme') {
+					vscode.workspace.getConfiguration("workbench").update(
+						"workbench.colorTheme",
+						"Abyss",
+						vscode.ConfigurationTarget.Global
+					);
+				}
+			}
+			);
+	}
+
+	getHtmlForWebview(): string {
+		return /* html */ `
+			<!DOCTYPE html>
+			<html lang="en">
+			<body>
+				<h2>Adaptive Theme Control</h2>
+				<button onclick="acquireVsCodeApi().postMessage({ command: 'switchTheme' })">
+        		  Switch Theme
+        		</button>
+			</body>
+			</html>
+		`;
+	}
+
+
+}
+
 export function deactivate() {}
